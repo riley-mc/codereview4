1c2364bbccba66335ff3bef84847c41f243df31c
Riley McAdoo <riley.mcadoo@gmail.com>
2020-11-06 11:13:01 -0800

    Added first radio list for options. started jst test.

---
+++ tests/scripts.test.js
@@ -0,0 +1,9 @@
+// Business Logic
+
+function Pizza(size, crust, sauce, toppings)  {
+  this.size = size;
+  this.crust = crust; 
+  this.sauce = sauce;
+  this.toppings = toppings;
+  this.price = 0;
+}
